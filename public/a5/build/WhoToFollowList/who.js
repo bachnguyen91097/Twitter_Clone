6f4861c0d0eb83ae2ee2647dbe91dd2b9bf6d577
Brian Nguyen <59807727+bachnguyen91097@users.noreply.github.com>
2021-10-11 18:56:11 -0400

    a5 JavaScript fa21

---
+++ public/a5/build/WhoToFollowList/who.js
@@ -0,0 +1,27 @@
+export default [
+    {
+        avatarIcon: '../../twitter/explore/images/java.png',
+        userName: 'Java',
+        handle: 'Java',
+    },
+    {
+        avatarIcon: '../../twitter/explore/images/relativity_space.jpeg',
+        userName: 'Relativity Space',
+        handle: 'relativityspace',
+    },
+    {
+        avatarIcon: '../../twitter/explore/images/virgin_ga.png',
+        userName: 'Virgin Galactic',
+        handle: 'virgingalactic',
+    },
+    {
+        avatarIcon: '../../twitter/explore/images/nasa.png',
+        userName: 'NASA',
+        handle: 'NASA',
+    },
+    {
+        avatarIcon: '../../twitter/explore/images/tesla.png',
+        userName: 'Tesla',
+        handle: 'Tesla',
+    },
+];
