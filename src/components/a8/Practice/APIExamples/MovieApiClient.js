626b3015ae7d8240a8769349cf03488ad9c1e88c
Brian Nguyen <59807727+bachnguyen91097@users.noreply.github.com>
2021-11-16 23:56:48 -0500

    Change to heroku URL

--- src/components/a8/Practice/APIExamples/MovieApiClient.js
+++ src/components/a8/Practice/APIExamples/MovieApiClient.js
@@ -18,19 +18,19 @@ const MovieApiClient = () => {
 
     const [movies, setMovies] = useState([]);
     const deleteMovie = (movie) =>
-        fetch(`http://localhost:4000/api/movies/${movie._id}`, {
+        fetch(`https://web-dev-node-bb.herokuapp.com/api/movies/${movie._id}`, {
             method: 'DELETE'
         })
             .then(response => response.json())
             .then(movies => setMovies(movies));
 
     useEffect(() =>
-            fetch('http://localhost:4000/api/movies')
+            fetch('https://web-dev-node-bb.herokuapp.com/api/movies')
                 .then(response => response.json())
                 .then(movies => setMovies(movies))
         , []);
     const saveMovie = () =>
-        fetch(`http://localhost:4000/api/movies/${movie._id}`, {
+        fetch(`https://web-dev-node-bb.herokuapp.com/api/movies/${movie._id}`, {
             method: 'PUT',
             body: JSON.stringify(movie),
             headers: {
